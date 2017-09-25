8339c6f92322e6cc707d05844eb5700d8c88ab19
Cedric Kisema <kisema@gmail.com>
2017-09-25 13:36:47 +0100

    nav bar collapses

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,8 @@
 console.log('\'Allo \'Allo!');
+
+$('#nav-menu-button').click(function() {
+  // document.getElementsByClassName('collapse navbar-collapse').css("display", "inline");
+  var comp = document.getElementsByClassName('collapse navbar-collapse')
+  console.log(comp);
+
+})
